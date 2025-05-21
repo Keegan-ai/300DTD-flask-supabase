3823c63e0dd3bafae5cb20933d6f91e405caff4e
Keegan-ai <kgous@waimea.school.nz>
2025-05-21 12:24:31 +1200

    Made a id link to only make one item pop up

---
+++ app/__init__.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import render_template
+from supabase import create_client
+from dotenv import load_dotenv
+import os
+
+#GEt Enviroment variables
+load_dotenv()
+SUPABASE_URL=os.getenv("SUPABASE_URL")
+SUPABASE_KEY=os.getenv("SUPABASE_KEY")
+
+#Connect to supabase as a client 
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+print(supabase)
+
+#Create Flask app 
+app = Flask(__name__)
+
+
+@app.get("/")
+def home():
+    response = supabase.table("things").select().order("name").execute()
+    records = response.data
+
+    return render_template("pages/home.jinja", things=records)
+
+@app.get("/thing/<int:id>")
+def showThing(id):
+    response = supabase.table("things").select().eq("id" , id).single().execute()
+    record = response.data
+
+    return render_template("pages/thing.jinja", thing=record)
+
+@app.get("/delete/<int:id>")
+def deleteThing(id):
+    # TODO!!!!!
+    return
+    
+@app.errorhandler(404)
+def notFound(error):
+    return render_template("pages/404.jinja")
